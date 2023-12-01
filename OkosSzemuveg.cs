f9d8cdc0703d50260760a9fffce167fe507adc05
B19BátonyiD <B19BtonyiD@204-07>
2023-12-01 13:26:13 +0100

    Add project files.

---
+++ OkosSzemuveg.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Okosszemuveg
+{
+    internal class OkosSzemuveg
+    {
+        public string sorszam { get; set; }
+        public double kijelzoMeret { get; set; }
+        public double procteljes { get; set; }
+        public int kameraTeljes { get; set; }
+        public string szenzorok { get; set; }
+        public string tarhely { get; set; }
+        public int uzemido { get; set; }
+        public double atvalt() => kameraTeljes * 2.54;
+        public int tarh() => int.Parse(tarhely.Split(" ")[0]);
+        public string[] szenz => szenzorok.Split(",");
+        public int szenkivalogatas() => int.Parse(szenzorok.Split(",")[0]);
+
+        public OkosSzemuveg(string sor)
+        {
+            string[] v = sor.Split(";");
+            sorszam = v[0];
+            kijelzoMeret = double.Parse(v[1]);
+            procteljes = double.Parse(v[2]);
+            kameraTeljes = int.Parse(v[3]);
+            szenzorok = v[4];
+            tarhely = v[5];
+            uzemido = int.Parse(v[6]);
+            
+        }
+
+        public override string ToString()
+        {
+            return $"\n Sorszám: {sorszam}, Kijelző mérete: {kijelzoMeret}, Processzor teljesítménye: {procteljes}, Kamera felbontása: {kameraTeljes}, Szenzorok: {szenzorok}, Tárhely mérete: {tarhely}, Üzemidő egy töltéssel: {uzemido} ";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿using System.Collections.Immutable;
+using System.ComponentModel.Design;
+using System.Net.Http.Headers;
+
+namespace Okosszemuveg
+{
+    internal class Program
+    {
+        static List<OkosSzemuveg> felbontas(List<OkosSzemuveg> a)
+        {
+            var f1 = a.Where(x => x.kameraTeljes >= 12 && x.procteljes == 2).ToList();
+            return f1;
+        }
+        static (List<OkosSzemuveg>,double) atlag(List<OkosSzemuveg> b)
+        {
+            var f2 = b.Average(x => x.uzemido);
+            var f2b = b.Where(x => x.uzemido > f2).ToList();
+            return (f2b,f2);
+        
+        }
+        static List<OkosSzemuveg> tarhely(List<OkosSzemuveg> c)
+        {
+            return c.Where(x => x.tarh() > 100).ToList();
+            
+        }    
+        static List<OkosSzemuveg> szenzor(List<OkosSzemuveg> d)
+        {
+            
+            return d.OrderBy(x => x.szenzorok).Distinct().ToList();
+        
+        }
+        static List<OkosSzemuveg> TB(List<OkosSzemuveg> e)
+        {
+           return e.Where(x => x.tarhely.Contains("TB")).ToList();
+        }
+        static List<OkosSzemuveg> szen(List<OkosSzemuveg> f)
+        { 
+        return f.Where(x => x.szenzorok.Count() > 3).ToList();
+        }
+        static List<string> many(List<OkosSzemuveg> g)
+        { 
+        
+            var f11 = g.SelectMany(x => x.szenz)
+                .Select(x => (x == "accelerometer") ? "gyorsulásmérő": x)
+                .Select(x => (x == "gyroscope") ? "giroszkóp" : x).OrderBy(x => x).Distinct().ToList();
+                
+
+            return f11;
+        }
+
+        static List<string> many2(List<OkosSzemuveg> h)
+        {
+            List<string> szenzorlista = new();
+            for (var i = 0; i < h.Count; i++)
+            {
+                for (int a = 0; a < h[i].szenz.Length; a++)
+                {
+                    if (h[i].szenz[a] == "accelerometer")
+                    {
+                        szenzorlista.Add("gyorsulásmérő");
+                    }
+                    else
+                    {
+                        if (h[i].szenz[a] == "gyroscope")
+                        {
+                            szenzorlista.Add("giroszkóp");
+                        }
+                        else 
+                        {
+                            szenzorlista.Add(h[i].szenz[a]);
+                        
+                        }
+                    }
+                    
+                }
+               
+            }
+
+            szenzorlista.Sort();
+            var szenzorlistadist = szenzorlista.Distinct().ToList();
+
+
+            return szenzorlistadist;
+        }
+        static void Main(string[] args)
+        {
+            var szemuveg = new List<OkosSzemuveg>();
+            using (var sr = new StreamReader(@"..\..\..\src\okosszemuvegek.txt"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    szemuveg.Add(new OkosSzemuveg(sr.ReadLine()));
+                }
+            
+            }
+
+            foreach (var item in szemuveg)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("7. feladat");
+            Console.WriteLine($"ennyi van: {felbontas(szemuveg).Count}");
+
+            Console.WriteLine("8. feladat");
+            (List<OkosSzemuveg> azAtlag, double f2) = atlag(szemuveg);
+            foreach (var item in azAtlag)
+            { Console.WriteLine(item);}
+            Console.WriteLine($"{f2}%");
+            Console.WriteLine($"{(azAtlag.Count)} db");
+            Console.WriteLine("10. feladat");
+            foreach (var item in tarhely(szemuveg))
+            {
+                Console.WriteLine($"Sorszama es meret: {item.sorszam} {item.atvalt()}");            
+            }
+            Console.WriteLine("11.feladat");
+            Console.WriteLine(szenzor(szemuveg));
+            Console.WriteLine("12. feladat");
+            if (TB(szemuveg).Count == 0)
+            {
+                Console.WriteLine("Nincs ilyen");
+
+            }
+            else
+            {
+                foreach (var item in TB(szemuveg))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+
+            using (var sw = new StreamWriter(@"..\..\..\src\kiiras.txt"))
+            {
+                foreach (var item in szen(szemuveg))
+                {
+                    sw.WriteLine(item);
+                }
+
+            }
+            many(szemuveg).ForEach(c => Console.WriteLine(c));
+            Console.WriteLine("2. megoldas");
+            many2(szemuveg).ForEach(c => Console.WriteLine(c));
+
+
+        }
+
+
+    }
+}
